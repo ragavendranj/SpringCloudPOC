4f120b93f2927e22891e3387e593846127be7e1a
ragavendran <ragavendran_j@infosys.com>
2018-03-08 14:39:52 -0700

    Updating

---
+++ SpringConfigCloudPOC/src/main/java/com/example/demo/SpringConfigCloudPocApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringConfigCloudPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigCloudPocApplication.class, args);
+	}
+}
+
+
